f5674aad9f847ac5f17fad4d92b584bd88d8665e
Karan Diwan <karandiwan46@gmail.com>
2026-09-16 17:12:34 +0530

    def to gds

--- CLA/gds/def_to_gds.rb
+++ CLA/gds/def_to_gds.rb
@@ -4,7 +4,7 @@ options = RBA::LoadLayoutOptions.new
 cfg = options.lefdef_config
 
 cfg.lef_files = [
-  "/home/kavon05/hdl/OpenROAD/test/sky130hd/sky130_fd_sc_hd_merged.lef"
+  "OpenROAD/test/sky130hd/sky130_fd_sc_hd_merged.lef"
 ]
 
 cfg.read_lef_with_def = true
@@ -13,10 +13,10 @@ options.lefdef_config = cfg
 
 layout = RBA::Layout.new
 layout.read(
-  "/mnt/c/Users/KARAN DIWAN/HPA/CLA/src/cla_final.def",
+  "cla.def",
   options
 )
 
 layout.write(
-  "/mnt/c/Users/KARAN DIWAN/HPA/CLA/src/cla_final.gds"
+  "cla.gds"
 )
